0bce068d454c9b28d47d927e482396433776748a
Nicholas Johnson <nicholas@forwardadvance.com>
2016-03-08 13:55:54 +0000

    exercises

--- demo_finished/demo.js
+++ demo_finished/demo.js
@@ -1,68 +1,10 @@
-// JavaScript does not have named parameters
-// Inject $scope, $log, $http
-// alert(fnc)
-// Minification safe square brace annotations ['$scope', function($scope){}]
-// ng-annotate
-// Injecting another module
-// ngSanitize
+angular.module('app', ['myModule', 'ngSanitize'])
+// myModule and ngSanitize have been included
 
-
-
-var fnc = function(b, a) {
-  return [a, b].join(' ');
-}
-
-// var a = "Hello";
-// var b = "World";
-// alert(fnc(a, b));
-
-angular.module('app', [])
+angular.module('myModule', [])
   .controller('myController', function($log, $scope) {
-    $scope.hello = "Hello from the Scope";
+    // Scope and log have been injected
+    $scope.hello = "<strong>Hello from the Scope</strong>";
     $log.log('hello from the log');
   })
   .service('myService', function() {})
-
-
-
-// angular.module('app', [])
-//   .controller('myController', ["$log", "$scope", function($log, $scope) {
-//     $scope.hello = "Hello from the Scope";
-//     $log.log('hello from the log');
-//   }])
-//   .service('myService', function() {})
-
-// var a = [
-//   "Hello",
-//   "From",
-//   "Angular"
-//   ].join(' ');
-
-// alert(a)
-
-
-
-
-
-
-
-
-
-
-// Injecting another module
-
-// Initial State
-
-// var fnc = function() {
-//   a = "Hello";
-//   b = "World";
-//   return [a, b].join(' ');
-// }
-// alert(fnc);
-
-// angular.module('app', [])
-//   .controller('myController', function() {
-// });
-
-
-
